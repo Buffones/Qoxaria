146152b0d136603bd18533a366237be68be10989
Nicolás Carol <53149680+n-ull@users.noreply.github.com>
2024-11-14 22:23:58 -0300

    Added simple design

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: unused_element
+
 import 'dart:convert';
 import 'dart:io';
 
@@ -9,13 +11,12 @@ import 'package:qoxaria/core/logger.dart';
 import 'package:qoxaria/core/models/version.dart';
 import 'package:qoxaria/features/auth/services/microsoft_auth.dart';
 import 'package:qoxaria/features/forge/widgets/forge_installation_widget.dart';
-
+import 'package:qoxaria/features/modpack/widgets/modpack_installation_widget.dart';
 
 void main() {
   setupLogging();
   runApp(const MyApp());
 }
-
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
@@ -25,8 +26,14 @@ class MyApp extends StatelessWidget {
     return ChangeNotifierProvider(
       create: (context) => MyAppState(),
       child: MaterialApp(
+        debugShowCheckedModeBanner: false,
         title: 'Qoxaria',
         theme: ThemeData(
+          textTheme: TextTheme(
+              bodyMedium: TextStyle(
+                  color: Colors.white,
+                  fontSize: 16,
+                  fontWeight: FontWeight.w600)),
           useMaterial3: true,
           colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepOrange),
         ),
@@ -36,7 +43,6 @@ class MyApp extends StatelessWidget {
   }
 }
 
-
 class MyAppState extends ChangeNotifier {
   late Future<QoxariaVersion> futureVersion;
 
@@ -45,16 +51,16 @@ class MyAppState extends ChangeNotifier {
   }
 
   Future<QoxariaVersion> fetchVersion() async {
-    final response = await http
-      .get(Uri.parse('https://qoxaria.buffones.com/version'));
+    final response =
+        await http.get(Uri.parse('https://qoxaria.buffones.com/version'));
     if (response.statusCode == 200) {
-      return QoxariaVersion.fromJson(jsonDecode(response.body) as Map<String, dynamic>);
+      return QoxariaVersion.fromJson(
+          jsonDecode(response.body) as Map<String, dynamic>);
     } else {
       throw Exception('Failed to load version.');
     }
   }
 }
-
 
 class MyHomePage extends StatelessWidget {
   const MyHomePage({super.key});
@@ -64,109 +70,232 @@ class MyHomePage extends StatelessWidget {
     var appState = context.watch<MyAppState>();
 
     return Scaffold(
-      body: Column(
-        children: [
-          Text(
-            'Qoxaria',
-            style: TextStyle(
-              fontSize: 32,
-              fontWeight: FontWeight.bold,
-            )
-          ),
-          FutureBuilder<QoxariaVersion>(
-            future: appState.futureVersion,
-            builder: (context, snapshot) {
-              if (!snapshot.hasData) {
-                return const Center(
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Text('Fetching version from the server...'),
-                      CircularProgressIndicator(),
-                    ],
-                  ),
-                );
-              }
+      body: Stack(children: [
+        Image.asset(
+          'assets/background.jpg',
+          fit: BoxFit.cover,
+          height: double.infinity,
+          width: double.infinity,
+        ),
+        Padding(
+          padding: const EdgeInsets.all(16),
+          child: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Image.asset('assets/logo.png', height: 75),
+                FutureBuilder<QoxariaVersion>(
+                    future: appState.futureVersion,
+                    builder: (context, snapshot) {
+                      if (!snapshot.hasData) {
+                        return const Center(
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Text('Fetching version from the server...'),
+                              CircularProgressIndicator(),
+                            ],
+                          ),
+                        );
+                      }
 
-              if (snapshot.hasError) {
-                return const Center(
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    children: [
-                      Text(
-                        textAlign: TextAlign.center,
-                        'Error fetching version from the server.\nApp not available.',
-                      ),
-                    ],
-                  )
-                );
-              }
+                      if (snapshot.hasError) {
+                        return const Center(
+                            child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          children: [
+                            Text(
+                              textAlign: TextAlign.center,
+                              'Error fetching version from the server.\nApp not available.',
+                            ),
+                          ],
+                        ));
+                      }
 
-              return Column(
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  SizedBox(
-                    width: 650,
-                    child: Padding(
-                      padding: const EdgeInsets.all(10),
-                      child: Wrap(
-                        crossAxisAlignment: WrapCrossAlignment.center,
-                        alignment: WrapAlignment.spaceBetween,
-                        spacing: 10.0,
+                      return Column(
+                        crossAxisAlignment: CrossAxisAlignment.center,
                         children: [
-                          Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              Text('Minecraft: ${snapshot.data!.minecraft}'),
-                              Text('Forge: ${snapshot.data!.forge}'),
-                            ],
+                          SizedBox(
+                            width: 650,
+                            child: Padding(
+                              padding: const EdgeInsets.all(10),
+                              child: Wrap(
+                                crossAxisAlignment: WrapCrossAlignment.center,
+                                alignment: WrapAlignment.spaceBetween,
+                                spacing: 10.0,
+                                children: [
+                                  Column(
+                                    crossAxisAlignment:
+                                        CrossAxisAlignment.start,
+                                    children: [
+                                      Text(
+                                          'Minecraft: ${snapshot.data!.minecraft}'),
+                                      Text('Forge: ${snapshot.data!.forge}'),
+                                    ],
+                                  ),
+                                  Column(
+                                    crossAxisAlignment:
+                                        CrossAxisAlignment.start,
+                                    children: [
+                                      Text(
+                                          'Modpack: ${snapshot.data!.modpack}'),
+                                    ],
+                                  ),
+                                ],
+                              ),
+                            ),
                           ),
-                          Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              Text('Modpack: ${snapshot.data!.modpack}'),
-                            ],
-                          ),
+
+                          ModpackInstallationWidget(version: snapshot.data!),
+
+                          //  ForgeInstallationWidget(version: snapshot.data!),
+                        ],
+                      );
+                    }),
+                /*           ElevatedButton(
+                  onPressed: () => MicrosoftAuth().authenticate(),
+                  child: Text('Login with Microsoft'),
+                ), */
+                ElevatedButton(
+                  onPressed: () async {
+                    final process = await Process.start(
+                      'cmd.exe',
+                      [
+                        '/c',
+                        'C:\\XboxGames\\Minecraft Launcher\\Content\\Minecraft.exe',
+                      ],
+                      // [],
+                      // ['-jar', installerFilePath, '--installClient', _getMinecraftDataPath()],
+                      mode: ProcessStartMode.normal,
+                    );
+                    process.stdout.transform(utf8.decoder).listen((data) {
+                      print(data);
+                    });
+
+                    process.stderr.transform(utf8.decoder).listen((data) {
+                      print(data);
+                    });
+                    int exitCode = await process.exitCode;
+                    print(exitCode);
+                  },
+                  child: Text('Open Launcher'),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ]),
+    );
+  }
+}
+
+class _MyHomePage extends StatelessWidget {
+  const _MyHomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    var appState = context.watch<MyAppState>();
+
+    return Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.all(16),
+        child: Column(
+          children: [
+            Image.asset('assets/logo.png', height: 75),
+            FutureBuilder<QoxariaVersion>(
+                future: appState.futureVersion,
+                builder: (context, snapshot) {
+                  if (!snapshot.hasData) {
+                    return const Center(
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Text('Fetching version from the server...'),
+                          CircularProgressIndicator(),
                         ],
                       ),
-                    ),
-                  ),
-                  ForgeInstallationWidget(version: snapshot.data!),
-                ],
-              );
-            }
-          ),
+                    );
+                  }
 
-          ElevatedButton(
-            onPressed: () => MicrosoftAuth().authenticate(),
-            child: Text('Login with Microsoft'),
-          ),
+                  if (snapshot.hasError) {
+                    return const Center(
+                        child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        Text(
+                          textAlign: TextAlign.center,
+                          'Error fetching version from the server.\nApp not available.',
+                        ),
+                      ],
+                    ));
+                  }
 
-          ElevatedButton(
-            onPressed: () async {
-              final process = await Process.start(
-                'cmd.exe',
-                ['/c', 
-                'C:\\XboxGames\\Minecraft Launcher\\Content\\Minecraft.exe',
-                ],
-                // [],
-                // ['-jar', installerFilePath, '--installClient', _getMinecraftDataPath()],
-                mode: ProcessStartMode.normal,
-              );
-              process.stdout.transform(utf8.decoder).listen((data) {
-                print(data);
-              });
+                  return Column(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      SizedBox(
+                        width: 650,
+                        child: Padding(
+                          padding: const EdgeInsets.all(10),
+                          child: Wrap(
+                            crossAxisAlignment: WrapCrossAlignment.center,
+                            alignment: WrapAlignment.spaceBetween,
+                            spacing: 10.0,
+                            children: [
+                              Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Text(
+                                      'Minecraft: ${snapshot.data!.minecraft}'),
+                                  Text('Forge: ${snapshot.data!.forge}'),
+                                ],
+                              ),
+                              Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Text('Modpack: ${snapshot.data!.modpack}'),
+                                ],
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                      ForgeInstallationWidget(version: snapshot.data!),
+                    ],
+                  );
+                }),
+            ElevatedButton(
+              onPressed: () => MicrosoftAuth().authenticate(),
+              child: Text('Login with Microsoft'),
+            ),
+            ElevatedButton(
+              onPressed: () async {
+                final process = await Process.start(
+                  'cmd.exe',
+                  [
+                    '/c',
+                    'C:\\XboxGames\\Minecraft Launcher\\Content\\Minecraft.exe',
+                  ],
+                  // [],
+                  // ['-jar', installerFilePath, '--installClient', _getMinecraftDataPath()],
+                  mode: ProcessStartMode.normal,
+                );
+                process.stdout.transform(utf8.decoder).listen((data) {
+                  print(data);
+                });
 
-              process.stderr.transform(utf8.decoder).listen((data) {
-                print(data);
-              });
-              int exitCode = await process.exitCode;
-              print(exitCode);
-            },
-            child: Text('Open Launcher'),
-          ),
-        ],
+                process.stderr.transform(utf8.decoder).listen((data) {
+                  print(data);
+                });
+                int exitCode = await process.exitCode;
+                print(exitCode);
+              },
+              child: Text('Open Launcher'),
+            ),
+          ],
+        ),
       ),
     );
   }
