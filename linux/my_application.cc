659f6cb672df2c1380831f37afc9a881164c3baf
Fran <francokuchiki@gmail.com>
2024-11-17 11:06:26 -0300

    chore: Change file name and install msix

--- linux/my_application.cc
+++ linux/my_application.cc
@@ -41,11 +41,11 @@ static void my_application_activate(GApplication* application) {
     GtkHeaderBar* header_bar = GTK_HEADER_BAR(gtk_header_bar_new());
     gtk_window_set_icon_from_file(GTK_WINDOW(window), "windows/runner/resources/app_icon.ico", NULL);
     gtk_widget_show(GTK_WIDGET(header_bar));
-    gtk_header_bar_set_title(header_bar, "qoxaria");
+    gtk_header_bar_set_title(header_bar, "Qoxaria");
     gtk_header_bar_set_show_close_button(header_bar, TRUE);
     gtk_window_set_titlebar(window, GTK_WIDGET(header_bar));
   } else {
-    gtk_window_set_title(window, "qoxaria");
+    gtk_window_set_title(window, "Qoxaria");
   }
 
   gtk_window_set_default_size(window, 1280, 720);
